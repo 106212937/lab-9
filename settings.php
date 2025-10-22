142793db7d5bfe55a2bf34af148189f41febdccb
ji-han-06 <106212937@student.swin.edu.au.com>
2025-10-22 10:49:02 +0800

    Added index.html

---
+++ settings.php
@@ -0,0 +1,7 @@
+<?php
+// Database connection details
+$host = "localhost";   // XAMPP server runs locally
+$user = "root";        // Default MySQL username
+$pwd  = "";            // Default MySQL password is empty
+$sql_db = "exhibition_db"; // <-- replace with your actual database name
+?>
